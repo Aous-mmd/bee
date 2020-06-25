fa96e53ca72c944bc5cf51069289a4ccbc95d799
aous.m <aous.m@aldrtechnology.com>
2020-06-25 09:45:45 -0400

    add all

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.slick').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
